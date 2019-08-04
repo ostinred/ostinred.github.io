544e637f0bec4365fe1796d630ffcb8bfbba6cef
Andrii Doroshev <ostinred@gmail.com>
2019-08-04 13:00:26 +0300

    new

--- js/main.js
+++ js/main.js
@@ -5,8 +5,17 @@ var input = document.querySelector('.is-input');
 var content = document.querySelector('.is-content');
 var img = document.querySelector('.img-opal');
 var typedString = document.querySelector('.was-sent');
+var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 
 window.onload = function (e) {
+  if (isSafari) {
+    document.querySelector('body').classList.add('isIos');
+  }
+
+  if (window.innerWidth >= 1170) {
+    input.setAttribute('autofocus', true);
+  }
+
   var validateEmail = function validateEmail(email) {
     var reExpress = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var result = reExpress.test(String(email).toLowerCase());
@@ -29,22 +38,22 @@ window.onload = function (e) {
     validateEmail(input.value);
     return;
   };
-};
 
-img.onclick = function () {
-  showTyped();
-};
-
-var showTyped = function showTyped() {
-  content.classList.add('is-hidden');
-  content.addEventListener('transitionend', function (e) {
-    e.target.style.display = 'none';
-    document.querySelector('#typed').style.display = 'block';
-    var typed = new Typed('#typed', {
-      stringsElement: '#typed-strings',
-      typeSpeed: 60,
-      smartBackspace: true,
-      fadeOut: true
+  var showTyped = function showTyped() {
+    content.classList.add('is-hidden');
+    content.addEventListener('transitionend', function (e) {
+      e.target.style.display = 'none';
+      document.querySelector('#typed').style.display = 'block';
+      var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 60,
+        smartBackspace: true,
+        fadeOut: true
+      });
     });
-  });
+  };
+
+  img.onclick = function () {
+    showTyped();
+  };
 };
